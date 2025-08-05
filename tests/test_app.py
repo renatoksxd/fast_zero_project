d05505ebae98a4a8067c2c8d83b1a600ec9aa77c
Renato Kenji Shiotoko <re_kenji@hotmail.com>
2025-08-05 17:37:54 -0300

    Iniciando o projeto

---
+++ tests/test_app.py
@@ -0,0 +1,15 @@
+from http import HTTPStatus
+
+from fastapi.testclient import TestClient
+
+from src.fast_zero.app import app
+
+
+def test_read_root_deve_retornar_ok_e_ola_mundo():
+    client = TestClient(app)  # arrange (organização)
+
+    response = client.get('/')  # act (ação)
+
+    assert response.status_code == HTTPStatus.OK  # assert (confirmação)
+    assert response.json() == {'message': 'Olá Mundo!'}  # assert
+    # (confirmação)
