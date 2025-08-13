965880d086f4141cfe113def6f8a0c2ad54887b7
Renato Kenji <re_kenji@hotmail.com>
2025-08-13 19:35:26 -0300

    Correções e exercícios atualizados

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,15 +1,25 @@
+from dataclasses import asdict
+
 from sqlalchemy import select
 
 from main.models import User
 
 
-def test_create_user(session):
-    user = User(
-        username='bob', password='minha_senha-legal', email='bob@gmail.com'
-    )
-    session.add(user)
-    session.commit()
+def test_create_user(session, mock_db_time):
+    with mock_db_time(model=User) as time:
+        new_user = User(
+            username='bob', password='minha_senha-legal', email='bob@gmail.com'
+        )
+        session.add(new_user)
+        session.commit()
 
-    result = session.scalar(select(User).where(User.email == 'bob@gmail.com'))
+        user = session.scalar(select(User).where(User.username == 'bob'))
 
-    assert result.id == 1
+    assert asdict(user) == {
+        'id': 1,
+        'username': 'bob',
+        'password': 'minha_senha-legal',
+        'email': 'bob@gmail.com',
+        'created_at': time,
+        'updated_at': time
+    }
